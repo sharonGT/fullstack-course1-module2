d6355b6ea280591be7e0405be371689b6a545261
Sharon Gozcu <sharongozcu@gmail.com>
2016-08-06 12:28:16 +0200

    fulfilled the requirement of removing if/else statements and inserting 'when' instead. Passed rspec requirements

--- Assignments/Lesson01-Assignment01-Case-Statement/student-start/test.rb
+++ Assignments/Lesson01-Assignment01-Case-Statement/student-start/test.rb
@@ -2,13 +2,13 @@ some_var = "false"
 another_var = "nil"
 
 case 
-when some_var == "pink elephant"
+ when some_var == "pink elephant"
   puts "Don't think about the pink elephant!"
 
-when another_var.nil?
+ when another_var.nil?
   puts "Question mark in the method name?"
 
-when some_var == false
+ when some_var == false
   puts "Looks like this one should execute"
 
 else
